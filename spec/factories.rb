2d22e908c12b76b25c7cb73e373e60aab5282136
SarCasm <sarcasm008@gmail.com>
2016-01-06 14:14:07 +0200

    Factory refactor

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :author do
-    name { Faker::Name.name }
-    desc { Faker::Lorem.sentence }
-    born { Faker::Date.between(60.years.ago, 20.years.ago) }
+    name    { Faker::Name.name }
+    desc    { Faker::Lorem.sentence }
+    born    { Faker::Date.between(60.years.ago, 20.years.ago) }
     country { Faker::Address.country }
   end
 
@@ -11,11 +11,16 @@ FactoryGirl.define do
   end
 
   factory :book do
-    title { Faker::Book.title }
-    desc  { Faker::Lorem.sentence }
-    quantity { rand(1..20) }
-    price { Faker::Number.decimal(2) }
-    category
-    author
+    transient do
+      category_count  1
+      authors_count   1
+    end
+
+    title       { Faker::Book.title }
+    desc        { Faker::Lorem.sentence }
+    quantity    { rand(1..20) }
+    price       { Faker::Number.decimal(2) }
+    categories  { build_list(:category, category_count) }
+    authors     { build_list(:author, authors_count) }
   end
 end
