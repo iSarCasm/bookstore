4c248d6c68823c653f122fddb63288687d390ecd
SarCasm <sarcasm008@gmail.com>
2016-01-08 01:38:06 +0200

    small controller refactor

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -3,11 +3,4 @@ class AuthorsController < ApplicationController
     @author = Author.find_by_id(params[:id])
     error_404 unless @author
   end
-
-  private
-
-  # wtf
-  def error_404
-    render file: "#{Rails.root}/public/404.html", layout: false, status: 404
-  end
 end
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,11 +3,4 @@ class BooksController < ApplicationController
     @book = Book.find_by_id(params[:id])
     error_404 unless @book
   end
-
-  private
-
-  # wtf
-  def error_404
-    render file: "#{Rails.root}/public/404.html", layout: false, status: 404
-  end
 end
