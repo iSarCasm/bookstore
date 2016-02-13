ea3ab40f98fde660c0420069e76b5edcfcd69400
SarCasm <sarcasm008@gmail.com>
2016-02-13 19:45:47 +0200

    Book #rating test

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -23,4 +23,19 @@ RSpec.describe Book, type: :model do
   it { should have_many(:authors).through(:book_owns) }
   it { should have_many(:categories).through(:category_books) }
   it { should have_many(:reviews) }
+
+  describe '#rating' do
+    it 'returns average rating of book from approved reviews' do
+      book = create(:book)
+      approved_reviews      = create_list(:review, 2, rating: 5, book: book, approved: true)
+      non_approved_reviews  = create_list(:review, 1, rating: 1, book: book)
+      expect(book.rating).to eq(5)
+    end
+
+    it 'returns 0 when no reviews yet' do
+      book = create(:book)
+      non_approved_reviews  = create_list(:review, 1, rating: 1, book: book)
+      expect(book.rating).to eq(0)
+    end
+  end
 end
