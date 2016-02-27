9a2c8fe9b0273fa2cb15921d84c4fdd7a96106d9
SarCasm <sarcasm008@gmail.com>
2016-02-27 21:22:41 +0200

    more checkouts

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -30,9 +30,17 @@ class CheckoutsController < ApplicationController
   end
 
   def place
-    @order.enqueue
-    @order.save
-    redirect_to @order
+    if address_invalid?
+      redirect_to action: :edit_address
+    elsif delivery_invalid?
+      redirect_to action: :edit_delivery
+    elsif payment_invalid?
+      redirect_to action: :edit_payment
+    else
+      @order.enqueue
+      @order.save
+      redirect_to @order
+    end
   end
 
   private
@@ -47,52 +55,67 @@ class CheckoutsController < ApplicationController
     fail 'You cant edit this one' unless @order.in_progress?
   end
 
-    def check_user_for!(order)
-      if (current_user != order.user)
-        fail "Permission denied."
-      end
-    end
+  def address_invalid?
+    !(@order.billing_address && @order.billing_address.valid?) ||
+    !(@order.shipment_address && @order.shipment_address.valid?)
+  end
 
-    def order_params
-      params.require(:order).permit(
-        :shipment_id,
-        billing_address_attributes: [
-          :id,
-          :first_name,
-          :last_name,
-          :street_address,
-          :city,
-          :country,
-          :zip,
-          :phone
-        ],
-        shipment_address_attributes: [
-          :id,
-          :first_name,
-          :last_name,
-          :street_address,
-          :city,
-          :country,
-          :zip,
-          :phone
-        ],
-        payment_attributes: [
-          :card,
-          :expiration_year,
-          :expiration_month,
-          :cvv
-        ]
-      )
-    end
+  def delivery_invalid?
+    !(@order.shipment)
+  end
 
-    def go_to_next_step
-      case params[:order][:step]
-      when 'address'
-        redirect_to edit_delivery_checkout_path(@order)
-      when 'shipment'
-        redirect_to edit_payment_checkout_path(@order)
-      when 'payment'
-        redirect_to confirm_checkout_path(@order)
-      end
+  def payment_invalid?
+    !(@order.payment && @order.payment.valid?)
+  end
+
+
+
+  def check_user_for!(order)
+    if (current_user != order.user)
+      fail "Permission denied."
     end
+  end
+
+  def order_params
+    params.require(:order).permit(
+      :shipment_id,
+      billing_address_attributes: [
+        :id,
+        :first_name,
+        :last_name,
+        :street_address,
+        :city,
+        :country,
+        :zip,
+        :phone
+      ],
+      shipment_address_attributes: [
+        :id,
+        :first_name,
+        :last_name,
+        :street_address,
+        :city,
+        :country,
+        :zip,
+        :phone
+      ],
+      payment_attributes: [
+        :card,
+        :expiration_year,
+        :expiration_month,
+        :cvv
+      ]
+    )
+  end
+
+  def go_to_next_step
+    case params[:order][:step]
+    when 'address'
+      redirect_to edit_delivery_checkout_path(@order)
+    when 'shipment'
+      redirect_to edit_payment_checkout_path(@order)
+    when 'payment'
+      redirect_to confirm_checkout_path(@order)
+    end
+  end
 end
