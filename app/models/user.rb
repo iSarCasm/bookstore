cf177f10cd3b1d7f0d5b9330a2b8a34b27378e2b
SarCasm <sarcasm008@gmail.com>
2016-02-28 14:54:13 +0200

    use email

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,7 +18,7 @@ class User < ActiveRecord::Base
 
 
   def self.create_with_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+    where(email: auth.info.email).first_or_create do |user|
       user.provider = auth.provider
       user.uid = auth.uid
       user.email = auth.info.email
