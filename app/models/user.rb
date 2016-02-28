52421e174adeecb9e7d00909c72575c5d7778464
SarCasm <sarcasm008@gmail.com>
2016-02-28 15:02:51 +0200

    sfdfsdf

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,12 +18,7 @@ class User < ActiveRecord::Base
 
 
   def self.create_with_omniauth(auth)
-    where(email: auth.info.email).first_or_create do |user|
-      user.provider = auth.provider
-      user.uid = auth.uid
-      user.email = auth.info.email
-      user.password = Devise.friendly_token[0,20]
-    end
+    where(email: auth.info.email).first
   end
 
   def billing_address
