00c99bed935df7cc6ec451a70dc4e37effe48671
SarCasm <sarcasm008@gmail.com>
2016-02-28 14:32:01 +0200

    work pls

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
-# config/initializers/omniauth.rb
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, 'fae28765b85d0f1e69ca', '55bda2d413833e43d0cc8401a61a3d7dbded6110', scope: "user:email,user:follow"
-end
+# # config/initializers/omniauth.rb
+# Rails.application.config.middleware.use OmniAuth::Builder do
+#   provider :github, 'fae28765b85d0f1e69ca', '55bda2d413833e43d0cc8401a61a3d7dbded6110', scope: "user:email,user:follow"
+# end
