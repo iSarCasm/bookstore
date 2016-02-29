f49195aae60ea167a9d5dbcae00e68e4fcac0163
SarCasm <sarcasm008@gmail.com>
2016-02-29 23:48:34 +0200

    id

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 # config/initializers/omniauth.rb
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'], scope: "user:email,user:follow"
+  provider :github, 'fae28765b85d0f1e69ca', '55bda2d413833e43d0cc8401a61a3d7dbded6110', scope: "user:email,user:follow"
 end
