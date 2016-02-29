1f4438c5bbd1f58c02f29f42096dd40332d7bb68
SarCasm <sarcasm008@gmail.com>
2016-03-01 00:03:40 +0200

    assdo

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,8 +19,8 @@ class User < ActiveRecord::Base
     where(provider: auth.provider, uid: auth.uid).first_or_create! do |user|
       user.provider = auth.provider
       user.uid = auth.uid
-      # user.email = auth.info.email
-      user.email = "LOL"
+      user.email = auth.info.email
+      user.email ||= "LOL@mail.lol"
       user.password = Devise.friendly_token[0,20]
     end
   end
