83b735ae029ff95eaeda824a30e0b2aabd27cde2
SarCasm <sarcasm008@gmail.com>
2016-04-06 21:13:40 +0300

    db change

--- db/migrate/20160325202931_return_prices.rb
+++
@@ -1,9 +0,0 @@
-class ReturnPrices < ActiveRecord::Migration
-  def change
-    Book.find_each do |book|
-      book.price = (10.50..60.30).step(0.01).to_a.sample
-      book.save
-    end
-    remove_column :books, :old_price, :integer
-  end
-end
